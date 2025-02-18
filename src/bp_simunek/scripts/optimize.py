2e6065e48ff6d628b6d23899a2b350c498c9dd2e
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-02-18 23:02:07 +0100

    Add runscript for pycma

---
+++ src/bp_simunek/scripts/optimize.py
@@ -0,0 +1,60 @@
+import logging
+import os
+from pathlib import Path
+import shutil
+import sys
+
+import ray
+
+from ..simulation.flow_wrapper import Wrapper
+from ..samplers.pycma_flow import PyCMAFlowWrapper
+from ..samplers.idata_tools import save_idata_to_file
+from ..plotting.flow_plots import generate_all_flow_plots
+
+from definitions import ROOT_DIR
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+def sample():
+    # probably not the best solution
+    # 107 char limit for socket path
+    #tmp_dir_symlink = os.path.join(os.path.expanduser("~"), ".r")
+    #logging.info(tmp_dir_symlink)
+    #if not os.path.islink(tmp_dir_symlink):
+    #    raise Exception("Missing symlink for Ray temp storage")
+    tmp_dir_mount = sys.argv[1]
+    cfg_path = sys.argv[2]
+    ray.init(_temp_dir=tmp_dir_mount)
+
+    os.chdir(script_dir)
+    observe_path = Path(cfg_path).absolute()
+    template_dir = Path(cfg_path).absolute()
+    workdir = os.environ.get("SCRATCHDIR")
+    if workdir is None:
+        work_dir = Path(ROOT_DIR, "output", "test12").absolute()
+    else:
+        work_dir = Path(os.path.join(workdir, "")).absolute()
+
+    logging.info("Using workdir %s", work_dir)
+
+    # copy template to workdir
+    shutil.copytree(template_dir, work_dir, dirs_exist_ok=True)
+
+    # init wrapper - load config
+    wrap = Wrapper(work_dir)
+
+    # add observe path to config
+    wrap.set_observe_path(observe_path)
+
+    # tinyda + flow123 wrapper
+    pycma_wrapper = PyCMAFlowWrapper(wrap)
+
+    # run sampling process
+    idata = pycma_wrapper.optimize()
+
+
+    # generate plots
+    generate_all_flow_plots(idata, folder=work_dir)
+
+if __name__ == "__main__":
+    sample()
