742179180a2e47d09feda63557c7a6c01f587297
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-04-28 10:00:06 +0200

    Revert to no-grad implementation of nn

--- src/bp_simunek/samplers/surrogates/flow_torch_wrapper.py
+++ src/bp_simunek/samplers/surrogates/flow_torch_wrapper.py
@@ -87,10 +87,14 @@ class Wrapper:
         #logging.info(self.parameters)
 
     def get_observations(self) -> npt.NDArray:
-        #with torch.no_grad():
-        parameters = torch.tensor(self.parameters, dtype=torch.float32, device=self.device)
-        y = self.model(parameters)
-        y = y.cpu().numpy() * 275.0
+        with torch.no_grad():
+            parameters = nn.Parameter(torch.tensor(self.parameters, dtype=torch.float32, device=self.device))
+            y = self.model(parameters)
+        #loss = y.sum()
+        #loss.backward()
+        #for name, param in self.model.named_parameters():
+        #    print(name, param.shape, param.grad.shape)
+        y = y.detach().cpu().numpy() * 275.0
         #return 1, np.concatenate([y[:, i::4] for i in range(4)], axis=1)
         data = [y[:, i::4] for i in range(4)]
         #logging.info(f"Surrogate data: {data}")
