c67aa5621204fb2335de71c620ed4262fdf12b01
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-02 20:40:17 +0200

    Check what directory we're in when arriving at worker node

--- scripts/worker_node_script.sh
+++ scripts/worker_node_script.sh
@@ -5,6 +5,8 @@ temp_dir=$SCRATCHDIR
 echo "tempdir: $temp_dir"
 echo `hostname -i`
 
+echo $(realpath .)
+
 cd $PBS_O_WORKDIR
 singularity instance start bp_simunek.sif cont
 singularity shell instance://cont
@@ -15,9 +17,9 @@ echo $(which python)
 
 
 #echo $(env | sort | grep RAY_PYTHON)
-"$RAY_PYTHON" -c pip list
-"$RAY_PYTHON" -m pip show ray
-"$RAY_PYTHON" -c "import ray; print(ray.__version__)"
+#"$RAY_PYTHON" -c pip list
+#"$RAY_PYTHON" -m pip show ray
+#"$RAY_PYTHON" -c "import ray; print(ray.__version__)"
 #"$RAY_PYTHON" -m ray.scripts.scripts start --address=$head_address --temp-dir=$temp_dir --num-cpus $PBS_NCPUS
 #echo $(env | sort | grep RAY_PYTHON)
 
