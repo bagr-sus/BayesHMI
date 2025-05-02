3ce4fb9cea105f91a453f77c5df070b586a78c0f
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-02 19:48:06 +0200

    More debug

--- scripts/worker_node_script.sh
+++ scripts/worker_node_script.sh
@@ -10,12 +10,15 @@ singularity instance start bp_simunek.sif cont
 singularity shell instance://cont scripts/worker_node_script.sh 
 source venv/bin/activate
 export RAY_PYTHON=$(which python)
-echo $(ls)
+echo $(ls $RAY_PYTHON)
+
 #echo $(env | sort | grep RAY_PYTHON)
 "$RAY_PYTHON" -c pip list
 "$RAY_PYTHON" -m pip show ray
 "$RAY_PYTHON" -c "import ray; print(ray.__version__)"
 #"$RAY_PYTHON" -m ray.scripts.scripts start --address=$head_address --temp-dir=$temp_dir --num-cpus $PBS_NCPUS
 #echo $(env | sort | grep RAY_PYTHON)
+
+cd $RAY_PYTHON
 exit;
 exit;
