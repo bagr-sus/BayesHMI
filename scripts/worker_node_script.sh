37edd7dd6f78034e0a4956192e380adeb25c6473
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-02 18:49:56 +0200

    add export to force ray into venv

--- scripts/worker_node_script.sh
+++ scripts/worker_node_script.sh
@@ -8,6 +8,7 @@ cd $PBS_O_WORKDIR &&
 singularity instance start bp_simunek.sif cont &&
 singularity shell instance://cont scripts/worker_node_script.sh &&
 source venv/bin/activate
+export RAY_PYTHON=$(which python)
 ray start --address=$head_address --temp-dir=$temp_dir --num-cpus $PBS_NCPUS
 exit;
 exit;
