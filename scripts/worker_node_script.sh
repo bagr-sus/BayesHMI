ace7302fbe54196a739b0474bf9c396be9345489
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-02 19:21:53 +0200

    force ray to use the correct python

--- scripts/worker_node_script.sh
+++ scripts/worker_node_script.sh
@@ -10,7 +10,7 @@ singularity shell instance://cont scripts/worker_node_script.sh &&
 source venv/bin/activate
 export RAY_PYTHON=$(which python)
 echo $(env | sort | grep RAY_PYTHON)
-ray start --address=$head_address --temp-dir=$temp_dir --num-cpus $PBS_NCPUS
+"$RAY_PYTHON" -m ray.scripts.scripts start --address=$head_address --temp-dir=$temp_dir --num-cpus $PBS_NCPUS
 echo $(env | sort | grep RAY_PYTHON)
 exit;
 exit;
