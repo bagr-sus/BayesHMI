e74f38aa3084f32e085ad1ca21986011df283b13
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-04 19:28:51 +0200

    Move debug outside of cont and fix typo

--- scripts/worker_cont_script.sh
+++ scripts/worker_cont_script.sh
@@ -13,9 +13,6 @@ echo $(which python)
 #"$RAY_PYTHON" -m pip show ray
 #"$RAY_PYTHON" -c "import ray; print(ray.__version__)"
 ray start --address=$head_address --num-cpus $PBS_NCPUS
-ray -m ray.scripts.scripts status
-
-cat /tmp/ray/session_latest/logs/raylet.err
-cat /tmp/ray/session_latest/logs/gcs_server.err
+ray status
 
 #echo $(env | sort | grep RAY_PYTHON)
