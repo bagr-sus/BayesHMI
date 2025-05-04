15b7630a29580526219cd4e06bb2563f267f6e58
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-04 20:09:12 +0200

    Copy existing pbs launch script to parallel one

--- scripts/parallel_test.sh
+++ scripts/parallel_test.sh
@@ -59,3 +59,50 @@ for (( n=$cores_per_node; n<$total_cores; n+=$cores_per_node )); do
     pbsdsh -n $n $worker_script_path $head_address &
     #pbsdsh -n $n 'echo 1'
 done
+
+# workers are blocked, so wait some time for them to start
+sleep 30
+
+cfg_path=$CFG_PATH
+
+id=$(echo "$PBS_JOBID" | cut -d'.' -f1)
+echo $id
+
+hash=$(echo "$id" | cksum)
+echo $hash
+
+link="${HOME}"/"${hash:0:3}"
+echo $link
+
+if [ ! -f "${link}" ] ; then
+    rm -r "${link}"
+fi
+ln -s $SCRATCHDIR $link
+
+SCRATCHDIR=$SCRATCHDIR  # Your temporary working directory
+TARGETDIR=$HOME/bayes_output/$PBS_JOBID  # Persistent backup location
+
+mkdir -p $TARGETDIR
+
+# Start periodic sync in the background
+while true; do
+    rsync -av --delete $SCRATCHDIR/ $TARGETDIR/
+    sleep 600  # Wait 5 minutes before syncing again
+done &
+SYNC_PID=$!  # Store the process ID of the background sync
+
+#cat "$(ls -la $SCRATCHDIR)"
+#cd $SCRATCHDIR
+
+cd $PBS_O_WORKDIR # potential cause of symlinks being overwritten
+singularity exec bp_simunek.sif bash scripts/singularity_run_script.sh "${link}" "${cfg_path}"
+
+# Kill the sync process when the job completes
+kill $SYNC_PID
+wait $SYNC_PID 2>/dev/null  # Ensure it's fully terminated
+
+# Perform a final sync to capture the last changes
+rsync -av $SCRATCHDIR/ $TARGETDIR/
+
+rm -r $link
+
