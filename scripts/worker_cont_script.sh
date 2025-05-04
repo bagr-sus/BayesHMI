3717f9c5b4070befafb884f01fc536bb29918929
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-04 19:18:45 +0200

    Add some new debug

--- scripts/worker_cont_script.sh
+++ scripts/worker_cont_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 head_address=$1
-temp_dir=$2
 
 echo `realpath .`
 echo "$SINGULARITY_CONTAINER"
@@ -12,5 +11,6 @@ echo $(which python)
 #"$RAY_PYTHON" -c pip list
 #"$RAY_PYTHON" -m pip show ray
 #"$RAY_PYTHON" -c "import ray; print(ray.__version__)"
-"$RAY_PYTHON" -m ray.scripts.scripts start --address=$head_address --temp-dir=$temp_dir --num-cpus $PBS_NCPUS
+"$RAY_PYTHON" -m ray.scripts.scripts start --address=$head_address --num-cpus $PBS_NCPUS
+"$RAY_PYTHON" -m ray.scripts.scripts status
 #echo $(env | sort | grep RAY_PYTHON)
--- scripts/worker_node_script.sh
+++ scripts/worker_node_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x
 head_address=$1
-temp_dir=$SCRATCHDIR
 echo "tempdir: $temp_dir"
 echo `hostname -i`
 
@@ -14,5 +13,5 @@ echo `realpath .`
 singularity instance start bp_simunek.sif contw
 echo $(singularity instance list)
 #singularity shell instance://contw
-singularity exec instance://contw scripts/worker_cont_script.sh $head_address $temp_dir
+singularity exec instance://contw scripts/worker_cont_script.sh $head_address
 exit;
