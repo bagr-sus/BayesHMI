dc24736ed5c566c2471de186eaa8368970229f51
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-04 19:25:59 +0200

    Add more debug

--- scripts/worker_cont_script.sh
+++ scripts/worker_cont_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 head_address=$1
 
 echo `realpath .`
@@ -11,6 +12,10 @@ echo $(which python)
 #"$RAY_PYTHON" -c pip list
 #"$RAY_PYTHON" -m pip show ray
 #"$RAY_PYTHON" -c "import ray; print(ray.__version__)"
-"$RAY_PYTHON" -m ray.scripts.scripts start --address=$head_address --num-cpus $PBS_NCPUS
-"$RAY_PYTHON" -m ray.scripts.scripts status
+ray start --address=$head_address --num-cpus $PBS_NCPUS
+ray -m ray.scripts.scripts status
+
+cat /tmp/ray/session_latest/logs/raylet.err
+cat /tmp/ray/session_latest/logs/gcs_server.err
+
 #echo $(env | sort | grep RAY_PYTHON)
