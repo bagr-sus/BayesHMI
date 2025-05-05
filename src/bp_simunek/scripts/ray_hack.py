2813d3102ebcb6ec2916d90ccc469f2b93482cb4
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-05-05 13:03:24 +0200

    bruh

--- src/bp_simunek/scripts/ray_hack.py
+++ src/bp_simunek/scripts/ray_hack.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 # Locate the real internal module
+import ray._private.node
 import ray._private.worker
+import ray._private.workers
 import ray._private.utils
 
 # Parse args from shell script
@@ -17,12 +19,12 @@ scratch = os.environ["SCRATCHDIR"]
 def short_session_dir(temp_dir: str) -> str:
     return os.path.join(temp_dir, "r")
 
-# Replace the internal function used during ray.init()
-ray._private.utils.get_user_temp_dir = short_session_dir
-
 # Use your actual SCRATCHDIR here
 ray.init(
     _temp_dir=os.environ["SCRATCHDIR"],
     num_cpus=num_cpus,
     logging_level="debug",
 )
+
+# Replace the internal function used during ray.init()
+ray._private.worker._global_node.session_name = "bruh"
