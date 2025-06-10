6c1e08dfda18ce2b4a74e5eda271ddd0a214b786
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-06-10 23:40:39 +0200

    Fix project root folder

--- src/bp_simunek/definitions.py
+++ src/bp_simunek/definitions.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).resolve().parent.parent # This is your Project Root
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent # This is your Project Root
